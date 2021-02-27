788b395ddbc8dbf3953941fec1fc8857fd551a9c
phildkim <philipdanielkim@gmail.com>
2021-02-27 02:19:24 -0800

    csun spring 2021

--- COMP_322L/Assignment_2/main.c
+++ COMP_322L/Assignment_2/main.c
@@ -1,136 +1,164 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
-/**
- *  repeat until Rᵢ == 0 for all n processes  // repeat until all processes have terminated
- *    while no process is active, increment t // if no process is ready to run, just advance t
- *    choose active processes pᵢ to run next
- *      according to scheduling algorithm     // Ex: FIFO, SJF, SRT
- *    decrement Rᵢ                            // pᵢ has accumulated 1 CPU time unit
- *    if Rᵢ == 0                              // process i has terminated
- *      set active flag of pᵢ = 0             // process i is excluded from further consideration
- *      TTᵢ = t - Aᵢ                          // the turnaround time of process i is the time
- *                                            // since arrival, TTᵢ, until the current time t
- *  compute the average turnaround time,
- *    ATT, by averaging all values TTᵢ
- **/
-/**
- * Print process, waiting time, turnaround time as a table
- **/
-void print(int arr[], int ser[], int n) {
-  printf(" _________________________________________ \n|   process   |   arrival   |   service   |\n");
-  for (int i=0;i<n;i++){
-    printf("|-------------|-------------|-------------|\n|      %c      |    %*dms     |    %*dms     |\n", 'A' + i, 2, arr[i], 2, ser[i]);
-  }
-  printf("-------------------------------------------\n");
-}
-/**
- * The FIFO (First-In-First-Out) algorithm schedules processes strictly according to the process arrival time.
- **/
+//  repeat until Rᵢ == 0 for all n processes    /* repeat until all processes have terminated */
+//    while no process is active, increment t   /* if no process is ready to run, just advance t */
+//    choose active processes pᵢ to run next
+//      according to scheduling algorithm       /* Ex: FIFO, SJF, SRT */
+//    decrement Rᵢ                              /* pᵢ has accumulated 1 CPU time unit */
+//    if Rᵢ == 0                                /* process i has terminated */
+//      set active flag of pᵢ = 0               /* process i is excluded from further consideration */
+//      TTᵢ = t - Aᵢ                            /* the turnaround time of process i is the time
+//                                                 since arrival, TTᵢ, until the current time t */
+//  compute the average turnaround time,
+//    ATT, by averaging all values TTᵢ
+struct process {
+  int arrival;
+  int service;
+};
 void fifo() {
-  int i, n, turnaround_total = 0;
-  printf("Enter the number of process: ");
+  int n, total = 0;
+  struct process fifo;
+  printf("\nNumber of process: ");
   scanf("%d", &n);
-  int arr[n];
-  int ser[n];
-  int waiting[n];
-  waiting[0] = 0;
-  for (i = 0; i < n; i++) {
-    printf("Process %c arrival time and service time: ", 'A' + i);
-    scanf("%d %d", &arr[i], &ser[i]);
-  }
-  for (i=1;i<n;i++)
-    waiting[i]=ser[i-1]+waiting[i-1];
-  for (i=0;i<n;i++)
-    turnaround_total += (ser[i] + waiting[i]) - arr[i];
-  print(arr, ser, n);
-  printf("   FIFO turnaround tot time: %dms", turnaround_total);
-  printf("\n   FIFO turnaround avg time: %.2fms\n\n", (float)turnaround_total / n);
-}
-/**
- * The SJF (Shortest Job First) algorithm schedules processes according to the total CPU time requirements.
- **/
+
+
+} // fifo schedules processes according to the process arrival time
 void sjf() {
-  int i, t, n, r = 0, min, turnaround_total = 0;
-  printf("Enter the number of process: ");
-  scanf("%d", &n);
-  int arr[n * 2 + 1];
-  int ser[n * 2 + 1];
-  int temp[n * 2 + 1];
-  int waiting[n];
-  for (i = 0; i < n; i++) {
-    printf("Process %c arrival time and service time: ", 'A' + i);
-    scanf("%d %d", &arr[i], &ser[i]);
-    temp[i] = ser[i];
-  }
-  ser[n*2] = INT_MAX;
-  for (t = 0; r != n; t++) {
-    min = n*2;
-    for (i = 0; i < n; i++) {
-      if (arr[i] <= t && ser[i] < ser[min] && ser[i] > 0)
-        min = i;
-    }
-    ser[min]--;
-    if (ser[min] == 0) {
-      r++;
-      turnaround_total += (t + 1) - arr[min];
-    }
-  }
-  print(arr, ser, n);
-  printf("   SJF turnaround tot time: %dms", turnaround_total);
-  printf("\n   SJF turnaround avg time: %.2fms\n\n", (float)turnaround_total / n);
-}
-/**
- * The SRT (Shortest Remaining Time) algorithm schedules processes according to the remaining CPU time needed to complete the work.
- **/
+  int total = 0;
+
+} // sjf schedules processes according to total CPU time requires
 void srt() {
-  int i, t, n, r = 0, min, turnaround_total = 0;
-  printf("Enter the number of process: ");
-  scanf("%d", &n);
-  int arr[n * 2 + 1];
-  int ser[n * 2 + 1];
-  int temp[n * 2 + 1];
-  int waiting[n];
-  for (i = 0; i < n; i++) {
-    printf("Process %c arrival time and service time: ", 'A' + i);
-    scanf("%d %d", &arr[i], &ser[i]);
-    temp[i] = ser[i];
+  int total = 0;
+
+} // srt schedules processes according to remaining CPU time needed to complete
+void prt(int arr[], int ser[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("\n|      %c      |      %*dms    |     %*dms    |", 'A' + i, 2, arr[i], 2, ser[i]);
   }
-  ser[n * 2] = INT_MAX;
-  for (t = 0; r != n; t++) {
-    min = n*2;
-    for (i = 0; i < n; i++) {
-      if (arr[i] <= t && ser[i] < ser[min] && ser[i] > 0)
-          min = i;
-    }
-    ser[min]--;
-    if (ser[min] == 0) {
-      r++;
-      turnaround_total += (t + 1) - arr[min];
-    }
-  }
-  print(arr, ser, n);
-  printf("   SRT turnaround tot time: %dms", turnaround_total);
-  printf("\n   SRT turnaround avg time: %.2fms\n\n", (float)turnaround_total / n);
-}
+} // print table of process, arrival time and service time
 
 int main(void) {
-  int n;
-  while (n != 4) {
-    printf("\nBatch scheduling algorithms:\n1. First-In-First-Out (FIFO)\n2. Shortest Job First (SJF)\n3. Shortest Remaining Time (SRT)\n4. Quit\nEnter algorithm #: ");
+  int n,p=0;
+  struct process = NULL;
+  while (n != 5) {
+    printf("\n\n1. FIFO\n2. SJF \n3. SRT \n4. All \n5. Quit\nEnter #: ");
     scanf("%d", &n);
+    prinf("No. of process: ");
+    scanf("%d", &p);
+
     switch (n) {
-      case 1:
-        fifo();
-        break;
-      case 2:
-        sjf();
-        break;
-      case 3:
-        srt();
-        break;
-      default:
-        break;
-      }
+    case 1:
+      printf("\nᵢ-----------------------------------------ᵢ"
+             "\n|    FIFO (First-In-First-Out) schedule   |"
+             "\n|-------------ᵢ-------------ᵢ-------------|"
+             "\n|   Process   |   Arrival   |   Service   |"
+             "\n|-------------|-------------|-------------|");
+      break;
+    case 2:
+      printf("\nᵢ-----------------------------------------ᵢ"
+             "\n|    SJF (Shortest Job First) schedule    |"
+             "\n|-------------ᵢ-------------ᵢ-------------|"
+             "\n|   Process   |   Arrival   |   Service   |"
+             "\n|-------------|-------------|-------------|");
+      break;
+    case 3:
+      printf("\nᵢ-----------------------------------------ᵢ"
+             "\n|  SRT (Shortest Remaining Time) schedule |"
+             "\n|-------------ᵢ-------------ᵢ-------------|"
+             "\n|   Process   |   Arrival   |   Service   |"
+             "\n|-------------|-------------|-------------|");
+      break;
+    case 4:
+      printf("\nᵢ-----------------------------------------ᵢ"
+             "\n|             FIFO, SJT, SRT              |"
+             "\n|-------------ᵢ-------------ᵢ-------------|"
+             "\n|   Process   |   Arrival   |   Service   |"
+             "\n|-------------|-------------|-------------|");
+      break;
+    default:
+      break;
+    }
   }
   return 0;
 }
+/**
+ *    ᵢ-----------------------------------------ᵢ
+ *    |    FIFO (First-In-First-Out) schedule   |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |   Process   |   Arrival   |   Service   |
+ *    |-------------|-------------|-------------|
+ *    |      A      |      0ms    |     18ms    |
+ *    |-------------|-------------|-------------|
+ *    |      B      |      2ms    |      7ms    |
+ *    |-------------|-------------|-------------|
+ *    |      C      |      2ms    |     10ms    |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |                   FIFO                  |
+ *    |      Total turnaround time:     74ms    |
+ *    |    Average turnaround time:  24.66ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *
+ *    ᵢ-----------------------------------------ᵢ
+ *    |    SJF (Shortest Job First) schedule    |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |   Process   |   Arrival   |   Service   |
+ *    |-------------|-------------|-------------|
+ *    |      A      |      3ms    |      5ms    |
+ *    |-------------|-------------|-------------|
+ *    |      B      |      0ms    |      4ms    |
+ *    |-------------|-------------|-------------|
+ *    |      C      |      4ms    |      2ms    |
+ *    |-------------|-------------|-------------|
+ *    |      D      |      5ms    |      4ms    |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |                   SJF                   |
+ *    |      Total turnaround time:     23ms    |
+ *    |    Average turnaround time:   5.75ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *
+ *    ᵢ-----------------------------------------ᵢ
+ *    |  SRT (Shortest Remaining Time) schedule |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |   Process   |   Arrival   |   Service   |
+ *    |-------------|-------------|-------------|
+ *    |      A      |      1ms    |      6ms    |
+ *    |-------------|-------------|-------------|
+ *    |      B      |      1ms    |      8ms    |
+ *    |-------------|-------------|-------------|
+ *    |      C      |      2ms    |      7ms    |
+ *    |-------------|-------------|-------------|
+ *    |      D      |      3ms    |      3ms    |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |                   SRT                   |
+ *    |      Total turnaround time:     51ms    |
+ *    |    Average turnaround time:  12.75ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *
+ *    ᵢ-----------------------------------------ᵢ
+ *    |             FIFO, SJT, SRT              |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |   Process   |   Arrival   |   Service   |
+ *    |-------------|-------------|-------------|
+ *    |      A      |      3ms    |      5ms    |
+ *    |-------------|-------------|-------------|
+ *    |      B      |      0ms    |      4ms    |
+ *    |-------------|-------------|-------------|
+ *    |      C      |      4ms    |      2ms    |
+ *    |-------------|-------------|-------------|
+ *    |      D      |      5ms    |      4ms    |
+ *    |-------------ᵢ-------------ᵢ-------------|
+ *    |                   FIFO                  |
+ *    |      Total turnaround time:     23ms    |
+ *    |    Average turnaround time:   5.75ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *    |                   SJF                   |
+ *    |      Total turnaround time:     23ms    |
+ *    |    Average turnaround time:   5.75ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *    |                   SRT                   |
+ *    |      Total turnaround time:     23ms    |
+ *    |    Average turnaround time:   5.75ms    |
+ *    ᵢ-----------------------------------------ᵢ
+ *
+ */
