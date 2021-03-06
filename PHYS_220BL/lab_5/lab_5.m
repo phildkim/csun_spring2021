5af5f4179bbed372f77bea771b5be9aa172bf92e
phildkim <philipdanielkim@gmail.com>
2021-03-06 11:40:59 -0800

    csun spring 2021

--- PHYS_220BL/lab_5/lab_5.m
+++ PHYS_220BL/lab_5/lab_5.m
@@ -1,5 +1,5 @@
 % Parallel
-R_ep=[0.34,0.90,0.18,0.12,0.08];
+R_ep=[0.34,0.92,0.16,0.08,0.0094];
 R_th=[0.33,0.91,0.18,0.09,0.0099];
 R_pp=R_ep./R_th;
 n=length(R_pp);
@@ -24,8 +24,8 @@ exportgraphics(h,'lab5_series.jpeg');
 
 
 
-R_ep=[1.08,0.086,1.38,1.42,1.97];
-R_th=[1.05,0.09524,1.4,1.6,1.96];
+R_ep=[1.08,0.0919,1.38,1.62,1.97];
+R_th=[1.05,0.09524,1.40,1.67,1.96];
 R_pp=R_ep./R_th;
 n=length(R_pp);
 R_slope=sum(R_pp)/n;
@@ -40,8 +40,8 @@ plot(R_th,R_ep,'sr',R_th,f,'-b','Linewidth',1.5);
 fontSize=18;
 set(gca,'fontSize',fontSize);
 title('Parallel Resistors: $R_{23,exp}$ vs. $R_{23,theory}$','Interpreter','latex');
-xlabel('$R_{23,exp}$','Interpreter','latex');
-ylabel('$R_{23,theory}$','Interpreter','latex');
+xlabel('$R_{23,theory}$','Interpreter','latex');
+ylabel('$R_{23,exp}$','Interpreter','latex');
 box on;
 hold off;
 exportgraphics(h,'lab5_parallel.jpeg');
