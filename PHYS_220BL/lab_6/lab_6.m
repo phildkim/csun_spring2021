095c18f2fb4d9941c295e384b10e9bfd52b17d56
phildkim <philipdanielkim@gmail.com>
2021-03-17 05:22:25 -0700

    csun spring 2021

--- PHYS_220BL/lab_6/lab_6.m
+++ PHYS_220BL/lab_6/lab_6.m
@@ -1,39 +1,59 @@
-VR=[0.89 1.94 1.94 2.27 2.43 2.51];
-VC=[2.5173 2.1824 1.8345 1.4057 1.1063 0.9104];
-VRC=[2.67 2.92 2.67 2.67 2.67 2.67];
-C=[.0000001 .0000022 .0000033 .0000047 .0000068 .000001];
-
-figure
-hold on
-plot(C,VR,'blue',C,VC,'red',C,VRC,'green');
-hold off
-title('Impedance of a Capacitor: $V_R, V_C, V_{RC}$ vs. C','Interpreter','latex');
-xlabel('Capacitor (C)');
-ylabel('Voltage ($V_R$, $V_C$, $V_{RC}$)','Interpreter','latex');
-legend('$V_R$', '$V_C$', '$V_{RC}$','Interpreter','latex');
+% Graph 1: VR vs. C, VC vs. C, VRC vs. C, Provide the legend so they can be distinguished
+% C=[10.00,4.55,3.03,2.13,1.47,1.00];
+% C=[0.10,0.22,0.33,0.47,0.68,1.00];
 
 
-
-R_ep=[1148.6,965.5,2929.4,611.2,461.0,364.2];
-R_th=[4.5455,3.0303,10,2.1277,1.4706,1.0000];
-R_pp=R_ep./R_th;
-n=length(R_pp);
-R_slope=sum(R_pp)/n;
-R_error=sqrt(sum(abs(R_pp-R_slope).^2)/(n-1));
-p=polyfit(R_th,R_ep,1);
-f=p(1)*R_th+p(2);
-[R_slope,R_error]% custom: slope +/- error
-[p(1),std(R_pp)]% check: slope +/- error
-h=figure(1);
+% ordered
+C=[0.10e-7,0.22e-7,0.33e-7,0.47e-7,0.68e-7,1.00e-6];
+VR=[0.81,1.54,2.02,2.27,2.43,2.51];
+VC=[2.54,2.18,1.75,1.41,1.11,0.91];
+VRC=[2.67,2.67,2.67,2.67,2.67,2.67];
+figure(1);
 hold on;
-plot(R_th,R_ep,'sr',R_th,f,'-b','Linewidth',1.5);
+box on;
+plot(C,VR,'-b',C,VC,'-r',C,VRC,'-g');
 fontSize=18;
 set(gca,'fontSize',fontSize);
-title('Impedance of a Capacitor: $X_{C,exp}$ vs. $\frac{1}{C}$','Interpreter','latex');
-xlabel('1/Capacitor ($\frac{1}{C}$)','Interpreter','latex');
-ylabel('($X_{C,exp}$)','Interpreter','latex');
-xlim([0.5,11]);
-ylim([0.5,3000]);
-box on;
+title('Impedance of a Capacitors: $V_R~vs.~C$, $V_C~vs.~C$, $V_{RC}~vs.~C$','Interpreter','latex');
+xlabel('Capacitors ($C\mu F$)','Interpreter','latex');
+ylabel('Voltage ($V_R$, $V_C$, $V_{RC}$)','Interpreter','latex');
+legend('$V_R$', '$V_C$', '$V_{RC}$','Interpreter','latex');
+xlim([0,1.1e-6]);
+ylim([0.5,3]);
 hold off;
-exportgraphics(h,'lab6_graph2.jpeg');
+
+
+
+
+% Graph 2: Xc,exp vs 1/C, fit it with a linear line and report the slope and error in the legend
+X=[1454.1,873.0,3140.9,611.2,460.9,364.2];
+C=[1/0.22,1/0.33,1/0.10,1/0.47,1/0.68,1/1.00];
+S=X./C;
+N=length(S);
+SLOPE=sum(S)/N;
+ERROR=sqrt(sum(abs(S-SLOPE).^2)/(N-1));
+P=polyfit(C,X,1);
+F=P(1)*C+P(2);
+[num2str(SLOPE) ' ' num2str(ERROR)]% Check Slope and Error
+[num2str(P(1)) ' ' num2str(std(S))]% Check Slope and Error
+figure(1);
+hold on;
+box on;
+plot(C,X,'sr',C,F,'-b');
+fontSize=18;
+set(gca,'fontSize',fontSize);
+title('Impedance of Capacitor: $X_{C,exp}$ vs $\frac{1}{C}$','Interpreter','latex');
+xlabel('Capacitors ($\frac{1}{C\mu F}$)','Interpreter','latex');
+ylabel('Experimental Reactance ($X_{C,exp}\Omega$)','Interpreter','latex');
+xlim([C(6)-0.5,C(3)+1]);
+ylim([X(1)-314.09,X(6)+36.42]);
+hold off;
+
+
+
+X=[1342.2,894.8,2952.8,628.3,434.2,295.3];
+C=[1/0.22,1/0.33,1/0.10,1/0.47,1/0.68,1/1.00];
+S=X./C;
+N=length(S);
+SLOPE=sum(S)/N;
+ERROR=sqrt(sum(abs(S-SLOPE).^2)/(N-1));
