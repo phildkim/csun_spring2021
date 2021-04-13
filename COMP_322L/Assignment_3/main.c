ea3e5ff76d2a9cb97942200516f01ee36bd5e53d
phildkim <philipdanielkim@gmail.com>
2021-04-13 10:42:48 -0700

    csun spring 2021

--- COMP_322L/Assignment_3/main.c
+++ COMP_322L/Assignment_3/main.c
@@ -1,57 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
-  int n, m;
-}*ptr = NULL;
-typedef struct node node;
 
-int *R;
-int **P;
-int p,r;
-void prt(int **arr) {
-  int i,j;
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < r; j++)
-      printf("%d ", arr[i][j]);
-    printf("\n");
-  }
-}
-void description() {
-  int i, j;
-  printf("\nDynamic Deadlock Avoidance using Banker’s Algorithm");
-  printf("\nThe number of processes:\t");
-  scanf("%d", &p);
-  printf("The number of resources:\t");
-  scanf("%d", &r);
-  R = (int *)malloc(r *sizeof(*R));
-  for (i = 0; i < r; i++) {
-    printf("The number of units for R[%d]:\t", i + 1);
-    scanf("%d", &R[i]);
-  }
-  P = (int **)malloc(p * sizeof(*P));
-  for (i = 0; i < p; i++)
-    P[i] = (int*)malloc(p *sizeof(**P));
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < r; j++) {
-      printf("The maximum claims of P[%d][%d]:\t", i + 1, j + 1);
-      scanf("%d", &P[i][j]);
-    }
-  }
-  prt(P);
-}
+int main(void) {
 
-// R[m] = number of units of each resource, m is number of resources
-// R[j] = number of units Rⱼ
-// P[n][m] = max claims
-// P[i][j] = max Rⱼ that Pᵢ requests
-int main() {
-  description();
   return 0;
 }
-/* 1) Develop an interactive program that first reads the description of a system from the command line or from a file. The description consists of the number of processes, the number of resources, the numbers of units within each resource, and the maximum claims of each process. Using the given information, the program creates the current representation of the system (the set of arrays). */
-
-/* 2) The program then enters an interactive session during which the user inputs commands of the form: request(i, j, k) or release(i, j, k), where i is a process number, j is a resource number, and k is the number of units of Rⱼ the process pᵢ is requesting or releasing. For each request operation, the program responds whether the request has been granted or denied. */
-
-/* The number of units of each resource is represented as a one-dimensional array R[m], where m is the number of resources and each entry R[j] records the number of units of resource Rⱼ. */
-
-/* The maximum claims are represented as a two-dimensional array P[n][m] where each entry P[i][j] contains an integer that records the maximum number of units of resource Rⱼ that process Pᵢ will ever request. */
---
+++ PHYS_220BL/lab_8/lab_8.m
@@ -0,0 +1,9 @@
+% High-Pass Filter Graph (H_exp vs. f, 'loglog') and (H_the, 'line')
+H_EXP=[];
+H_THE=[];
+F_OSC=[];
+
+% Low-Pass Filter Graph (H_exp vs. f, 'loglog') and (H_the, 'line')
+H_EXP=[];
+H_THE=[];
+F_OSC=[];
