3eea8ef2555ba1c8df7b342db306409de5503410
phildkim <philipdanielkim@gmail.com>
2021-04-24 10:04:27 -0700

    csun spring 2021

---
+++ PHYS_220BL/lab_9/lab_9.m
@@ -0,0 +1,17 @@
+% Graph 1: 4 subplots for the 4 datasets for the 4 different resistors, plot V_dischage vs t as a scatterplot in a semilogarithmic plot, using semilogy in matlab/GNU octave.
+
+
+
+t=[];
+V_dischage1 = [];
+V_dischage2 = [];
+V_dischage3 = [];
+V_dischage4 = [];
+
+
+
+% Graph 2: plot τ vs R as a scatterplot, fit this with a straight line, draw the fit as a lineplot in the same graph and report the slope.
+
+τ=[];
+R=[];
+
