c6cff295f3051f7c74ccb71ee06c94bfc52e3045
phildkim <philipdanielkim@gmail.com>
2021-04-27 00:27:29 -0700

    csun spring 2021

--- BIO_106L/Lab_10/lab_10.m
+++ BIO_106L/Lab_10/lab_10.m
@@ -1,16 +1,16 @@
 M = [-4.23, 3.5, 2.82, 3.73, 2.98, 0.168, 1.24, -0.16, 2.14, 0.18, 1.06, -1.37, 0.28, 2.84, 1.18, -0.038, 2.18];
-% N_M = length(M);
-% MEAN_M = sum(M) / N_M;
-% STD_M = sqrt(sum(abs(M-MEAN_M).^2)/(N_M-1));
-% SE_M = STD_M./sqrt(N_M);
-% CI_M = 2 * SE_M;
+N_M = length(M);
+MEAN_M = sum(M) / N_M;
+STD_M = sqrt(sum(abs(M-MEAN_M).^2)/(N_M-1));
+SE_M = STD_M./sqrt(N_M);
+CI_M = 2 * SE_M;
 
 F = [-2.25, -1.5, 1.78, 2.97, 3.46, 3.82, -1.14, -1.12, 0.56, -0.92, 3.2, 0.2, -0.38, 2.12, 2.46, 3.25, 2.18];
-% N_F = length(F);
-% MEAN_F = sum(F) / N_F;
-% STD_F = sqrt(sum(abs(F-MEAN_F).^2)/(N_F-1));
-% SE_F = STD_F./sqrt(N_F);
-% CI_F = 2 * SE_F;
+N_F = length(F);
+MEAN_F = sum(F) / N_F;
+STD_F = sqrt(sum(abs(F-MEAN_F).^2)/(N_F-1));
+SE_F = STD_F./sqrt(N_F);
+CI_F = 2 * SE_F;
 
 Na = length(M);
 Nb = length(F);
