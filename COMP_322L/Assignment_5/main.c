7d00083cb7a0b9202fdc1f273268ac127a977c66
phildkim <philipdanielkim@gmail.com>
2021-04-28 04:45:46 -0700

    csun spring 2021

--- COMP_322L/Assignment_5/main.c
+++ COMP_322L/Assignment_5/main.c
@@ -32,6 +32,7 @@ void init(char **argv) {
 		fscanf(sys, "%d", &ptr->p[i]);
 		ptr->pf[i] = 0;
 	}
+	fclose(sys);
 }
 void reset() {
 	int i;
@@ -49,25 +50,30 @@ int main(int argc, char **argv) {
 	int i, j, id, loc,
 	min_frag, max_frag,
 	in_frag = 0, ex_frag = 0;
-
+	FILE *sys = fopen("result.txt", "w");
 	// initialize
 	init(argv);
 
 	// first fit
 	printf("\nFIRST-FIT");
+	fprintf(sys,"\nFIRST-FIT");
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
 			if (ptr->p[i] <= ptr->b[j] && ptr->bf[j] == 0 && ptr->pf[i] == 0) {
 				ptr->bf[j] = ptr->pf[i] = 1;
 				in_frag += ptr->b[j] - ptr->p[i];
 				printf("\nP[%d]:\tB[%d]", i, j);
+				fprintf(sys, "\nP[%d]:\tB[%d]", i, j);
 				break;
 			}
 		}
-		if (ptr->pf[i] == 0)
+		if (ptr->pf[i] == 0) {
 			printf("\nP[%d]:\tNot Allocated", i);
+			fprintf(sys, "\nP[%d]:\tNot Allocated", i);
+		}
 	}
 	printf("\nINTERNAL FRAG: %d", in_frag);
+	fprintf(sys, "\nINTERNAL FRAG: %d", in_frag);
 	for(j=0;j<m;j++){
 		if(ptr->bf[j] == 0)
 			ex_frag+=ptr->b[j];
@@ -75,9 +81,13 @@ int main(int argc, char **argv) {
 	printf("\nEXTERNAL FRAG: %d", ex_frag);
 	printf("\nMEM UTILITIES: %0.2f", (float)in_frag/n);
 	printf("\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
+	fprintf(sys, "\nEXTERNAL FRAG: %d", ex_frag);
+	fprintf(sys, "\nMEM UTILITIES: %0.2f", (float)in_frag / n);
+	fprintf(sys, "\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
 	// best fit
 	reset();
 	printf("\n\nBEST-FIT");
+	fprintf(sys, "\nBEST-FIT");
 	for (i = 0; i < n; i++) {
 		min_frag = INT_MAX;
 		id = INT_MAX;
@@ -91,11 +101,14 @@ int main(int argc, char **argv) {
 			ptr->bf[id] = ptr->pf[i] = 1;
 			in_frag += ptr->b[id] - ptr->p[i];
 			printf("\nP[%d]:\tB[%d]", i, id);
+			fprintf(sys, "\nP[%d]:\tB[%d]", i, id);
 		}
-		if (ptr->pf[i] == 0)
-			printf("\nP[%d]:-\tNot Allocated", i);
+		if (ptr->pf[i] == 0) {
+			fprintf(sys, "\nP[%d]:-\tNot Allocated", i);
+		}
 	}
 	printf("\nINTERNAL FRAG: %d", in_frag);
+	fprintf(sys, "\nINTERNAL FRAG: %d", in_frag);
 	for(j=0;j<m;j++){
 		if(ptr->bf[j] == 0)
 			ex_frag+=ptr->b[j];
@@ -103,9 +116,13 @@ int main(int argc, char **argv) {
 	printf("\nEXTERNAL FRAG: %d", ex_frag);
 	printf("\nMEM UTILITIES: %0.2f", (float)in_frag / n);
 	printf("\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
+	fprintf(sys, "\nEXTERNAL FRAG: %d", ex_frag);
+	fprintf(sys, "\nMEM UTILITIES: %0.2f", (float)in_frag / n);
+	fprintf(sys, "\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
 	// worst fit
 	reset();
 	printf("\n\nWORST-FIT");
+	fprintf(sys, "\nWORST-FIT");
 	for(i=0;i<n;i++) {
 		max_frag = -1;
 		id=INT_MAX;
@@ -116,14 +133,19 @@ int main(int argc, char **argv) {
 			}
 		}
 		if(max_frag != -1){
-				ptr->bf[id] = ptr->pf[i] = 1;
-				in_frag += ptr->b[id] - ptr->p[i];
-				printf("\nP[%d]:\tB[%d]",i,id);
+			ptr->bf[id] = ptr->pf[i] = 1;
+			in_frag += ptr->b[id] - ptr->p[i];
+			printf("\nP[%d]:\tB[%d]",i,id);
+			fprintf(sys, "\nP[%d]:\tB[%d]", i, id);
 		}
-		if (ptr->pf[i] == 0)
+		if (ptr->pf[i] == 0) {
 			printf("\nP[%d]:\tNot Allocated", i);
+			fprintf(sys, "\nP[%d]:\tNot Allocated", i);
+		}
+
 	}
 	printf("\nINTERNAL FRAG: %d", in_frag);
+	fprintf(sys, "\nINTERNAL FRAG: %d", in_frag);
 	for(j=0;j<m;j++){
 		if(ptr->bf[j] == 0)
 			ex_frag+=ptr->b[j];
@@ -131,25 +153,34 @@ int main(int argc, char **argv) {
 	printf("\nEXTERNAL FRAG: %d", ex_frag);
 	printf("\nMEM UTILITIES: %0.2f", (float)in_frag / n);
 	printf("\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
+	fprintf(sys, "\nEXTERNAL FRAG: %d", ex_frag);
+	fprintf(sys, "\nMEM UTILITIES: %0.2f", (float)in_frag / n);
+	fprintf(sys, "\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
 	// next fit
 	reset();
 	printf("\nEnter block no. to begin allocation: ");
 	scanf("%d", &loc);
 	printf("NEXT-FIT");
+	fprintf(sys, "\nNEXT-FIT");
 	for (i = 0; i < n; i++) {
 		for (j = loc; j < m; j++) {
 			if (ptr->p[i] <= ptr->b[j] && ptr->bf[j] == 0 && ptr->pf[i] == 0) {
 				ptr->bf[j] = ptr->pf[i] = 1;
 				in_frag += ptr->b[j] - ptr->p[i];
 				printf("\nP[%d]:\tB[%d]", i, j);
+				fprintf(sys, "\nP[%d]:\tB[%d]", i, j);
 				loc = ((j + 1) == m) ? 0 : j+1;
 				break;
 			}
 		}
-		if (ptr->pf[i] == 0)
+		if (ptr->pf[i] == 0) {
 			printf("\nP[%d]:\tNot Allocated", i);
+			fprintf(sys, "\nP[%d]:\tNot Allocated", i);
+		}
+
 	}
 	printf("\nINTERNAL FRAG: %d",in_frag);
+	fprintf(sys, "\nINTERNAL FRAG: %d", in_frag);
 	for(j=0;j<m;j++){
 		if(ptr->bf[j] == 0)
 			ex_frag+=ptr->b[j];
@@ -157,6 +188,10 @@ int main(int argc, char **argv) {
 	printf("\nEXTERNAL FRAG: %d", ex_frag);
 	printf("\nMEM UTILITIES: %0.2f", (float)in_frag / n);
 	printf("\nSEARCH TIMES:  %0.2f\n", (float)ex_frag/m);
+	fprintf(sys, "\nEXTERNAL FRAG: %d", ex_frag);
+	fprintf(sys, "\nMEM UTILITIES: %0.2f", (float)in_frag / n);
+	fprintf(sys, "\nSEARCH TIMES:  %0.2f\n", (float)ex_frag / m);
+	fclose(sys);
 	collection();
 	return 0;
 }
