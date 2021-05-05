a6a494f6510001212a72c5361f2178b73d81fa7a
phildkim <philipdanielkim@gmail.com>
2021-05-05 01:41:39 -0700

    csun spring 2021

---
+++ PHYS_220BL/lab_12/lab_12.m
@@ -0,0 +1,3 @@
+% plot Zₗ² vs f², linear fit and from the offset and slope, determine R_int and L. Provide a legend for the data and fit
+f=[1000^2, 22000^2,32000^2,39000^2,45000^2,50000^2,55000^2,60000^2,65000^2];
+z=[];
